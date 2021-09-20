ceb6574dcea0a862ff3513020df05eebb94efd4f
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-09-19 23:18:02 -0500

    initial commit

---
+++ src/pages/SkeletonApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/SkeletonApp.module.css';
+
+export const SkeletonApp = () => {
+    return (
+        <div className={styles.app}>
+            <h1>Hola mundo</h1>
+        </div>
+    );
+};
