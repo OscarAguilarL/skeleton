4867a1b13b9e107e11531b7083c31a9ff059cc91
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-09-20 09:35:10 -0500

    add: import images and create data variable

--- src/pages/SkeletonApp.js
+++ src/pages/SkeletonApp.js
@@ -1,10 +1,23 @@
 import React from 'react';
+
+import { Card } from '../components/Card';
+import { CardContainer } from '../components/CardContainer';
 import { Header } from '../components/Header';
+import { Wrapper } from '../components/Wrapper';
+
+import { posts } from '../utils/data';
 
 export const SkeletonApp = () => {
   return (
     <>
       <Header />
+      <Wrapper>
+        <CardContainer>
+          {posts.map((post) => (
+            <Card key={post.title} {...post} />
+          ))}
+        </CardContainer>
+      </Wrapper>
     </>
   );
 };
---
+++ src/utils/data.js
@@ -0,0 +1,83 @@
+import image1 from '../assets/1.png';
+import image2 from '../assets/2.png';
+import image3 from '../assets/3.png';
+import image4 from '../assets/4.png';
+import image5 from '../assets/5.png';
+import image6 from '../assets/6.png';
+import image7 from '../assets/7.png';
+import image8 from '../assets/8.png';
+import image9 from '../assets/9.png';
+
+import leonidas from '../assets/leonidas.png';
+import carlos from '../assets/carlos.png';
+import uxcristopher from '../assets/uxcristopher.png';
+import humberto from '../assets/humberto.png';
+import sandra from '../assets/sandra.png';
+import mafer from '../assets/mafer-mazu.png';
+import miguel from '../assets/miguel.png';
+
+export const posts = [
+  {
+    image: image1,
+    title: 'De Figma a CSS en un click con este plugin',
+    profilePic: leonidas,
+    name: 'Leonidas',
+    readTime: '8 min',
+  },
+  {
+    image: image2,
+    title: 'Toma fotografías que luzcan profesionales con tu teléfono.',
+    profilePic: carlos,
+    name: 'Carlos',
+    readTime: '6 min',
+  },
+  {
+    image: image3,
+    title: '3 libros para comenzar en UX-UI',
+    profilePic: uxcristopher,
+    name: 'uxcristopher',
+    readTime: '8 min',
+  },
+  {
+    image: image4,
+    title: 'Becas por estudiar',
+    profilePic: humberto,
+    name: 'Humberto',
+    readTime: '8 min',
+  },
+  {
+    image: image5,
+    title: 'Más que un color bonito',
+    profilePic: sandra,
+    name: 'Sandra',
+    readTime: '8 min',
+  },
+  {
+    image: image6,
+    title: 'El misterio de las APIs',
+    profilePic: mafer,
+    name: 'Mafer Mazu',
+    readTime: '8 min',
+  },
+  {
+    image: image7,
+    title: 'Hola Cypress',
+    profilePic: miguel,
+    name: 'Miguel',
+    readTime: '8 min',
+  },
+  {
+    image: image8,
+    title: 'Qué hay detrás de flex:1;',
+    profilePic: leonidas,
+    name: 'Leonidas',
+    readTime: '8 min',
+  },
+  {
+    image: image9,
+    title: 'Desearás haber comenzado hoy',
+    profilePic: humberto,
+    name: 'Humberto',
+    readTime: '8 min',
+  },
+];
